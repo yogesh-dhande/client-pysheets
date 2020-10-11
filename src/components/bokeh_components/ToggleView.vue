51df6d5ed087c5357600c990382731b68295354f
Yogesh Dhande <yogeshkd@gmail.com>
2020-10-11 05:07:16 +0000

    Updated NPM packages

--- src/components/bokeh_components/ToggleView.vue
+++ src/components/bokeh_components/ToggleView.vue
@@ -2,7 +2,7 @@
     <div>
         <b-form-checkbox switch
                          :id="model.id"
-                         v-model="model.active"
+                         v-model="localModel.active"
                          size="lg"
         > {{model.label}}
         </b-form-checkbox>
@@ -13,12 +13,17 @@
     export default {
         name: "ToggleView",
         props: ["model"],
+        data () {
+            return {
+                localModel: this.model
+            }
+        },
         computed: {
             localIcon () {
                 let modifier = this.model.icon? this.model.icon.icon_name: "refresh"
                 console.log(modifier)
                 return "fa fa-" + modifier
-            }
+            },
         }
     }
 </script>
